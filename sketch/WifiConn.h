b8754da635fa81c6f08c4898e2011bd5c331f3db
Relderf <relderfeg@gmail.com>
2025-05-18 12:52:32 -0300

    Timeout de conexión Wifi

--- sketch/WifiConn.h
+++ sketch/WifiConn.h
@@ -6,13 +6,14 @@
 class WifiConn {
 private:
     WiFiClientSecure clienteWifi;
+    int attempts = 0;
+    const int maxAttempts = 15;
 
 public:
     WifiConn();
     WiFiClientSecure& getCliente();
     void connect(String ssid, String password);
+    bool isConnected();
 };
-
-
 
 #endif //WIFICONN_H
