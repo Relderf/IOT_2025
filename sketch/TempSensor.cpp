e7816e7de7d9db836229d7f959cae7ed19046051
Relderf <relderfeg@gmail.com>
2025-06-07 11:06:11 -0300

    Simulador de temperatura lineal

--- sketch/TempSensor.cpp
+++ sketch/TempSensor.cpp
@@ -18,12 +18,41 @@ bool TempSensor::temperaturaValida() {
     return !isnan(ultimaTemperatura);
 }
 
+float simularTemperatura() {
+    // La primera vez que se llama a esta función, se simula una temperatura 
+    //aleatoria entre 10 y 35 grados Celsius. Despues de eso, crece hacia 
+    //el extremo que tenga más lejos (10 o 35) de a 1 cada 5 segundos, 
+    //y cuando llega al límite, vuelve para el otro lado.
+    static float temperaturaSimulada = 0;
+    static unsigned long ultimaLecturaSimuladaMs = 0;
+    static bool creciendo = true;
+    if ((millis() - ultimaLecturaSimuladaMs) > 5000) {
+        ultimaLecturaSimuladaMs = millis();
+        if (temperaturaSimulada == 0) {
+            temperaturaSimulada = random(10, 35);
+        } else {
+            if (creciendo) {
+                temperaturaSimulada += 1;
+                if (temperaturaSimulada >= 35) {
+                    creciendo = false;
+                }
+            } else {
+                temperaturaSimulada -= 1;
+                if (temperaturaSimulada <= 10) {
+                    creciendo = true;
+                }
+            }
+        }
+    }
+    return temperaturaSimulada;
+}
+
 void TempSensor::checkTemperatura() {
     if ((millis() - ultimaLecturaTempMs) > intervaloLecturaTempMs) {
         ultimaLecturaTempMs = millis();
  		float temperatura;
         if (TEMP_MOCK) {
-            temperatura = random(18, 36);
+            temperatura = simularTemperatura();
         } else {
             temperatura = dht.readTemperature();
         }
